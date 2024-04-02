80f5654a01a614d4a33a5c3269aabf2bf9f5a37d
mxnnmax <ljm123634@gmail.com>
2024-04-03 00:50:54 +0900

    Feat: cs-center(total)

---
+++ src/Components/CsCenterContent.js
@@ -0,0 +1,88 @@
+import styled from "styled-components"
+import { useState } from "react";
+
+const CsCenterContent=({type, contentData})=>{
+    const [showContent, setShowContent]=useState(null);
+    const handleShowContent=(id)=>{
+        setShowContent(id===showContent?null:id);
+    }
+
+    return(
+        <NotificationContainer>
+            {contentData.map(item=>
+                <div key={item.id}>
+                    <NotificationElement>
+                        <div>
+                            <NotificationTag>{type}</NotificationTag>
+                            <NotificationTitle>{item.title}</NotificationTitle>
+                        </div>
+                        <NotificationButton onClick={(e)=>handleShowContent(item.id)}>{showContent===item.id?"닫기":"보기"}</NotificationButton>
+                    </NotificationElement>
+                    {showContent===item.id&&<NotificationContentContainer>
+                        <NotificationContent>{item.content}</NotificationContent>
+                    </NotificationContentContainer>}
+                </div>
+            )}
+        </NotificationContainer>
+    )
+}
+
+const NotificationContainer=styled.div`
+    height: 450px;
+    overflow-y: scroll;
+`
+
+const NotificationElement=styled.div`
+    background-color: #ffffff;
+    padding: 20px;
+    border-radius: 10px;
+    margin-bottom: 15px;
+    display: flex;
+    justify-content: space-between;
+
+    @media (max-width:450px){
+        padding: 10px;
+    }
+`
+
+const NotificationTag=styled.h5`
+    margin: 0px;
+    display: inline-block;
+    color: #979797;
+    margin-right: 20px;
+    cursor: default;
+
+    @media (max-width:450px){
+        margin-right: 10px;
+    }
+`
+
+const NotificationTitle=styled.h5`
+    margin: 0px;
+    display: inline-block;
+    font-weight: normal;
+    cursor: default;
+`
+
+const NotificationButton=styled.button`
+    border: 0;
+    padding: 5px 10px;
+    display: block;
+    background-color: #d9d9d9;
+    border-radius: 5px;
+    cursor: pointer;
+`
+
+const NotificationContentContainer=styled.div`
+    padding: 20px;
+    border-radius: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #979797;
+`
+
+const NotificationContent=styled.h5`
+    margin: 0px;
+    font-weight: normal;
+`
+
+export default CsCenterContent;
--- src/Pages/CsCenter.js
+++ src/Pages/CsCenter.js
@@ -1,13 +1,44 @@
 import styled from 'styled-components';
 import { useState } from 'react';
-import CsCenterSideBar from '../Components/CsCenterSideBar';
 import Notification from '../Components/Notification';
+import Event from '../Components/Event';
+import ProductRegistrationInquiry from '../Components/ProductRegistrationInquiry';
+import ServiceInformation from '../Components/ServiceInformation';
+import OtherInquiry from '../Components/OtherInquiry';
 
 const CsCenter=()=>{
+    const [menu, setMenu]=useState(0);
+
+    const handleMenu=(menu)=>{
+        setMenu(menu);
+    }
+
+    const showContent=()=>{
+        switch(menu){
+            case 0: return(<Notification handleMenu={handleMenu}></Notification>);
+            case 1: return(<Event handleMenu={handleMenu}></Event>)
+            case 2: return(<ServiceInformation handleMenu={handleMenu}></ServiceInformation>)
+            case 3: return(<ProductRegistrationInquiry handleMenu={handleMenu}></ProductRegistrationInquiry>)
+            case 4: return(<OtherInquiry handleMenu={handleMenu}></OtherInquiry>)
+            default: return(<Notification handleMenu={handleMenu}></Notification>);
+        }
+    }
+
     return(
         <CsCenterContainer>
-            <CsCenterSideBar></CsCenterSideBar>
-            <Notification></Notification>
+            <NavigationContainer>
+                <NavigationTitle>고객센터</NavigationTitle>
+                <CategoryContainer>
+                    <NavigationElement onClick={(e)=>{handleMenu(0)}}>공지사항</NavigationElement>
+                    <NavigationElement onClick={(e)=>{handleMenu(1)}}>이벤트</NavigationElement>
+                    <NavigationElement onClick={(e)=>{handleMenu(2)}}>서비스 안내</NavigationElement>
+                </CategoryContainer>
+                <CategoryContainer>
+                    <NavigationElement onClick={(e)=>{handleMenu(3)}}>상품 등록 문의</NavigationElement>
+                    <NavigationElement onClick={(e)=>{handleMenu(4)}}>기타 문의</NavigationElement>
+                </CategoryContainer>
+            </NavigationContainer>
+            {showContent()}
         </CsCenterContainer>
     )
 }
@@ -16,6 +47,31 @@ const CsCenterContainer=styled.div`
     margin: 0px 10%;
     margin-top: 70px;
     display: flex;
+`
+
+const NavigationContainer=styled.div`
+    margin-right: 50px;
+
+    @media (max-width:600px){
+        display: none;
+    }
+`
+
+const NavigationTitle=styled.h3`
+    margin: 0px;
+    margin-bottom: 20px;
+    cursor: default;
+`
+
+const CategoryContainer=styled.div`
+    margin-bottom: 15px;
+`
+
+const NavigationElement=styled.h5`
+    margin: 0px;
+    margin-bottom: 10px;
+    font-weight: normal;
+    cursor: pointer;
 `
 
 export default CsCenter;
