be6c025da38e76168a2c8a5e13525d15871acc59
PMS990126 <barkminsung@naver.com>
2024-05-10 17:24:13 +0900

    t

--- src/Pages/certification.js
+++ src/Pages/certification.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default function Certification() {
+export default function Certification() { 
 
     const location = useLocation(); // useLocation 훅
     const navigate = useNavigate(); // 페이지 이동 훅
