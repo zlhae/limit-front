d63815f5ec990de675f1cf612e7ffe31269b13ec
mxnnmax <ljm123634@gmail.com>
2024-06-04 21:36:03 +0900

    main

--- src/Pages/certification.js
+++ src/Pages/certification.js
@@ -36,7 +36,7 @@ export default function Certification() {
                     } 
                 }
             };
-            CertificationEmail();
+            CertificationEmail(); 
         }
     }, [certificationToken, email]);
 
